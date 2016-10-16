031387ee0b84522d124c6060446f2d6f05efd2d7
Hari <hariomhari@hotmail.com>
2016-10-16 21:43:37 +0100

    Added Console App

---
+++ WholegameApp/Wholegame.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wholegame.Core.Repositories;
+using Wholegame.Core.Services.Data;
+
+namespace Wholegame.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PlayerDataService playerDataService = new PlayerDataService(new PlayerRepository());
+			var players = playerDataService.SearchPlayers("Ian").Result;
+
+			ClubPlayerDataService clubPlayerDataService = new ClubPlayerDataService(new ClubPlayerRepository(),new PlayerRepository());
+			var clubPlayers = clubPlayerDataService.GetClubPlayers(1).Result;
+
+			Console.WriteLine(clubPlayers.Count());
+
+			Console.ReadLine();
+
+
+		}
+	}
+}
